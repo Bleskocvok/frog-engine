fc17d72489ea35f511d5c27c3b2f20be975de68f
František Bráblík <frantisek.brablik@gmail.com>
2024-02-04 15:05:23 +0100

    Versatile build script

--- script/build.sh
+++ script/build.sh
@@ -2,9 +2,40 @@
 
 # Run this script from the root folder of the project:
 # (the folder where CMakeLists.txt is located)
-#     $ script/build.sh
+#     $ script/build.sh build game2d Debug
 
-cmake -D CMAKE_BUILD_TYPE="$1"  \
-      -S . -B bin-lin        \
-    && cmake --build bin-lin -j4
+set -e
 
+[ "$#" -lt 2 ] && { echo "Usage: $0 [-o out_dir] [-b build_type] cmd dir"; exit 1; }
+
+cmd="$1"
+dir="$2"
+build_type="$3"
+out="bin-${dir}-lin"
+
+build()
+{
+    cmake --build "$out" -j4
+}
+
+gen()
+{
+    cmake -D CMAKE_BUILD_TYPE="$build_type" -S "$dir" -B "$out"
+}
+
+case "$cmd" in
+    build)
+        build
+        ;;
+    gen)
+        gen
+        ;;
+    gen-build)
+        gen
+        build
+        ;;
+    *)
+        echo "Ivalid cmd: $cmd"
+        exit 1
+        ;;
+esac
