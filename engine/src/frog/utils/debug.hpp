d717f377ee87b6da36188079c8a1e8bb77e33271
František Bráblík <frantisek.brablik@gmail.com>
2024-10-09 02:08:52 +0200

    utils: Log macro works on containers

--- engine/src/frog/utils/debug.hpp
+++ engine/src/frog/utils/debug.hpp
@@ -5,21 +5,47 @@
 #define LOGX(x) frog::log_ln(#x, "→", x)
 #define LOG(...) frog::log_ln(__VA_ARGS__ )
 
+
 namespace frog
 {
 
+
 template<typename Arg, typename ... Args>
-inline void log_ln(Arg&& arg, Args&& ... args)
+void log(Arg&& arg, Args&& ... args)
 {
-    if constexpr (sizeof...(Args) == 0)
+    if constexpr (requires{ arg.begin(); arg.end(); })
     {
-        std::clog << arg << '\n';
+        if (arg.begin() != arg.end())
+        {
+            std::clog << "[ ";
+            const char* del = "";
+            for (auto it = arg.begin(), end = arg.end(); it != end; ++it)
+            {
+                std::clog << del;
+                del = ", ";
+                log(*it);
+            }
+            std::clog << " ]";
+        }
+        else
+            std::clog << "[]";
     }
     else
+        std::clog << arg;
+
+    if constexpr (sizeof...(Args) != 0)
     {
-        std::clog << arg << " ";
-        log_ln(std::forward<Args>(args)...);
+        std::clog << " ";
+        log(std::forward<Args>(args)...);
     }
+}
+
+
+template<typename ... Args>
+void log_ln(Args&& ... args)
+{
+    log(std::forward<Args>(args)...);
+    std::clog << "\n";
 }
 
 
