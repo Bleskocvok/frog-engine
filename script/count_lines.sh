d24673910ac7bfee525f0fdf03736b25c02524d6
František Bráblík <frantisek.brablik@gmail.com>
2024-12-30 10:49:36 +0100

    CI: Setup LoC calculation

--- script/count_lines.sh
+++ script/count_lines.sh
@@ -1,11 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-#files="src/ engine/src/ CMakeLists.txt engine/CMakeLists.txt engine/tests"
+lines="$(cloc --quiet --csv engine/src/ | tail -n1 | cut -d, -f5)"
 
-files="$(git ls-files | fgrep -v libraries)"
-
-cloc $files
-
-for i in $(seq 80); do printf '#'; done
-
-scc $files
+LC_ALL="C.UTF-8" awk -v lines="$lines" 'BEGIN {printf "%6.1fk\n", lines/1000}' | sed 's/^ *//'
