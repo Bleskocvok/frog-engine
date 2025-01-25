ee4b1980ed1305511f4d660e5b4fa91d42ac4c14
František Bráblík <frantisek.brablik@gmail.com>
2025-01-25 16:53:23 +0100

    Feat(scripts): Function to build frame around button

---
+++ engine/src/frog/scripts/button_utils.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "frog/graphics/ui_element.hpp"
+#include "frog/utils/ptr.hpp"
+
+namespace frog {
+
+template<typename GameObject>
+void build_frame(frog::geo::rect rect, GameObject& obj,
+                 const std::string& image_tag, float around = 0)
+{
+    using namespace frog::geo;
+
+    const vec2 tex_size = { 5 / 32.0, 5 / 16.0 };
+    const vec2 elem_size = { 0.02, 0.02 };
+
+    rect.size -= elem_size;
+    rect.size += around * elem_size * 2;
+
+    auto* t  = obj.add_element(frog::mk_ptr<frog::gx::ui_element>());
+    auto* b  = obj.add_element(frog::mk_ptr<frog::gx::ui_element>());
+    auto* l  = obj.add_element(frog::mk_ptr<frog::gx::ui_element>());
+    auto* r  = obj.add_element(frog::mk_ptr<frog::gx::ui_element>());
+    auto* tl = obj.add_element(frog::mk_ptr<frog::gx::ui_element>());
+    auto* tr = obj.add_element(frog::mk_ptr<frog::gx::ui_element>());
+    auto* bl = obj.add_element(frog::mk_ptr<frog::gx::ui_element>());
+    auto* br = obj.add_element(frog::mk_ptr<frog::gx::ui_element>());
+
+    for (auto* elem : { t, b, l, r, tl, tr, bl, br })
+    {
+        elem->sprite.image_tag = image_tag;
+        elem->sprite.rect.size = elem_size;
+        elem->sprite.tex.size = tex_size;
+    }
+
+    t->sprite.rect.pos = 0.5 * ( rect.top_left() + rect.top_right() );
+    t->sprite.rect.size = { rect.size.x() - elem_size.x(), elem_size.y() };
+    t->sprite.tex.size = { 1 - tex_size.x() * 2, tex_size.y() };
+    t->sprite.tex.pos = { tex_size.x(), 0 };
+
+    b->sprite.rect.pos = 0.5 * ( rect.bot_left() + rect.bot_right() );
+    b->sprite.rect.size = { rect.size.x() - elem_size.x(), elem_size.y() };
+    b->sprite.tex.size = { 1 - tex_size.x() * 2, tex_size.y() };
+    b->sprite.tex.pos = { tex_size.x(), 1 - tex_size.y() };
+
+    l->sprite.rect.pos = 0.5 * ( rect.top_left() + rect.bot_left() );
+    l->sprite.rect.size = { elem_size.x(), rect.size.y() - elem_size.y() };
+    l->sprite.tex.size = { tex_size.x(), 1 - tex_size.y() * 2 };
+    l->sprite.tex.pos = { 0, tex_size.y() };
+
+    r->sprite.rect.pos = 0.5 * ( rect.top_right() + rect.bot_right() );
+    r->sprite.rect.size = { elem_size.x(), rect.size.y() - elem_size.y() };
+    r->sprite.tex.size = { tex_size.x(), 1 - tex_size.y() * 2 };
+    r->sprite.tex.pos = { 1 - tex_size.x(), tex_size.y() };
+
+    tl->sprite.rect.pos = rect.top_left();
+
+    tr->sprite.rect.pos = rect.top_right();
+    tr->sprite.tex.pos = { 1 - tex_size.x(), 0 };
+
+    bl->sprite.rect.pos = rect.bot_left();
+    bl->sprite.tex.pos = { 0, 1 - tex_size.y() };
+
+    br->sprite.rect.pos = rect.bot_right();
+    br->sprite.tex.pos = { 1 - tex_size.x(), 1 - tex_size.y() };
+}
+
+} // namespace frog
