31554d3dc2118766a5fe90c76142fe93eec61cb5
František Bráblík <frantisek.brablik@gmail.com>
2025-02-16 18:14:37 +0100

    CI: Avoid dependency on someone's cloc

--- script/count_lines.sh
+++ script/count_lines.sh
@@ -2,6 +2,11 @@
 
 set -eo pipefail
 
-lines="$(cloc --quiet --csv engine/src/ | tail -n1 | cut -d, -f5)"
+
+ARGS=". --exclude-dir=libraries,.github --vcs git"
+
+1>&2 cloc $ARGS
+
+lines="$(cloc --quiet --csv $ARGS | tail -n1 | cut -d, -f5)"
 
 LC_ALL="C.UTF-8" awk -v lines="$lines" 'BEGIN {printf "%6.1fk\n", lines/1000}' | sed 's/^ *//'
