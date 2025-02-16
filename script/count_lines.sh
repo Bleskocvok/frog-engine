7bc017178da3cbe586bd813af32fb01d8812e123
František Bráblík <frantisek.brablik@gmail.com>
2025-02-16 18:44:27 +0100

    CI: Fix loc pt. 2

--- script/count_lines.sh
+++ script/count_lines.sh
@@ -9,4 +9,5 @@ ARGS=". --exclude-dir=libraries,.github --vcs git"
 
 lines="$(cloc --quiet --csv $ARGS | tail -n1 | cut -d, -f5)"
 
-LC_ALL="C.UTF-8" awk -v lines="$lines" 'BEGIN {printf "%6.1fk\n", lines/1000}' | sed 's/^ *//'
+# LC_ALL="C.UTF-8" awk -v lines="$lines" 'BEGIN {printf "%6.1fk\n", lines/1000}' | sed 's/^ *//'
+echo "$lines"
