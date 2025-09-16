68bc6374e36618909fb1f07627aeec5a17573efd
František Bráblík <frantisek.brablik@gmail.com>
2025-09-16 20:32:50 +0200

    Fix(utils): Assert shows basename of file

--- engine/src/frog/utils/assert.hpp
+++ engine/src/frog/utils/assert.hpp
@@ -2,10 +2,20 @@
 
 #include "exception.hpp"
 
+#include <filesystem>
+
+inline std::string basename(const std::string& path)
+{
+    if (path.find('/') != path.npos)
+        return std::filesystem::path(path).filename().string();
+
+    return path;
+}
+
 // This one stays even with NDEBUG
 #define frog_assert( expr ) \
     do {                    \
         if ( not ( expr )) \
-            throw frog::error( "assert failed ", __FILE__, ":", __LINE__,   \
+            throw frog::error( "assert failed ", basename( __FILE__ ), ":", __LINE__,   \
                             ": frog_assert( ", #expr, " )" );               \
     } while (false)
