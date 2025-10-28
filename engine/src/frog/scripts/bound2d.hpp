f6ea6698996250880241800cd31b3652bc246ee5
František Bráblík <frantisek.brablik@gmail.com>
2025-10-28 22:03:54 +0100

    Feat(scripts): Bound2d script

---
+++ engine/src/frog/scripts/bound2d.hpp
@@ -0,0 +1,84 @@
+#pragma once
+
+#include "frog/core/script.hpp"
+#include "frog/core/engine2d.hpp"
+#include "frog/core/game_object2d.hpp"
+#include "frog/geometry/circle.hpp"
+#include "frog/geometry/general.hpp"
+#include "frog/geometry/physics.hpp"
+#include "frog/geometry/vector.hpp"
+#include "frog/gx2d/sprite.hpp"
+
+#include <cmath>
+#include <optional>
+#include <utility>      // move
+
+namespace frog::scripts {
+
+class bound2d : public frog::script2d
+{
+    frog::geo::soft_physics2d* physics = nullptr;
+    frog::geo::soft_physics2d::idx_t idx = -1;
+    frog::geo::soft_physics2d::idx_t idx_add = -1;
+
+    frog::geo::soft_physics2d::idx_t idx_rot_a = -1;
+    frog::geo::soft_physics2d::idx_t idx_rot_b = -1;
+
+    float rotation_delta_deg = 0;
+
+public:
+    bound2d(frog::geo::soft_physics2d& physics)
+        : physics(&physics)
+    { }
+
+    void bind(frog::geo::soft_physics2d::idx_t i)
+    {
+        idx = i;
+    }
+
+    void bind_between(frog::geo::soft_physics2d::idx_t a,
+                      frog::geo::soft_physics2d::idx_t b)
+    {
+        idx = a;
+        idx_add = b;
+    }
+
+    void bind_rotation(frog::geo::soft_physics2d::idx_t a,
+                       frog::geo::soft_physics2d::idx_t b)
+    {
+        idx_rot_a = a;
+        idx_rot_b = b;
+    }
+
+    void rotation_delta(float deg)
+    {
+        rotation_delta_deg = deg;
+    }
+
+    void init(frog::game_object2d& o, frog::engine2d&) override
+    {
+    }
+
+    void end_update(frog::game_object2d& obj, frog::engine2d&) override
+    {
+        if (idx_add != -1 && idx != -1)
+        {
+            const auto& a = physics->point_at(idx);
+            const auto& b = physics->point_at(idx_add);
+
+            obj.model().rect.pos = frog::geo::lerp(a.pos, b.pos, 0.5f);
+            obj.model().angle = geo::angle_deg(a.pos, b.pos);
+        }
+        else if (idx != -1)
+            obj.model().rect.pos = physics->point_at(idx).pos;
+
+        if (idx_rot_a != -1 && idx_rot_b != -1)
+        {
+            const auto& a = physics->point_at(idx);
+            const auto& b = physics->point_at(idx_add);
+            obj.model().angle = rotation_delta_deg + geo::angle_deg(a.pos, b.pos);
+        }
+    }
+};
+
+} // namespace frog::scripts
