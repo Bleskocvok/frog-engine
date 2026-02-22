89076f7751e01703d478014eef548f380262475b
František Bráblík <frantisek.brablik@gmail.com>
2026-02-22 19:13:46 +0100

    Feat(gx): Color from hex

--- engine/src/frog/graphics/color.hpp
+++ engine/src/frog/graphics/color.hpp
@@ -1,8 +1,11 @@
 #pragma once
 
 #include "frog/geometry/vector.hpp"
+#include "frog/utils/exception.hpp"
 
 #include <cstdint>      // uint8_t
+#include <stdexcept>
+#include <string_view>
 
 
 namespace frog::gx
@@ -25,6 +28,50 @@ inline geo::vec4 rgb_to_vec( gx::rgba_t rgb )
              rgb.a() / 255.0f };
 }
 
+namespace color {
+
+inline constexpr rgba_t from_hex(std::string_view str)
+{
+    using error = std::runtime_error;
+
+    if (str.starts_with('#'))
+        str.remove_prefix(1);
+
+    if (str.size() != 6)
+        throw error("from_hex: string.size() != 6");
+
+    constexpr std::string_view hexes_up  = "0123456789ABCDEF";
+    constexpr std::string_view hexes_low = "0123456789abcdef";
+
+    auto find = [&](auto c)
+    {
+        auto idx = hexes_up.find(c);
+        if (idx != hexes_up.npos)
+            return idx;
+
+        idx = hexes_low.find(c);
+        if (idx != hexes_low.npos)
+            return idx;
+
+        throw error("from_hex: invalid string");
+    };
+
+    auto get_color = [&]()
+        -> std::uint8_t
+    {
+        auto i = find(str[0]);
+        auto j = find(str[1]);
+        str.remove_prefix(2);
+        return i * 16 + j;
+    };
+
+    auto r = get_color();
+    auto g = get_color();
+    auto b = get_color();
+    return { r, g, b, 255 };
+}
+
+} // namespace color
 
 namespace colors
 {
