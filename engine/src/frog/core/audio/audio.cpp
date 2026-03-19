fba8a2b5ea29b2d8bfb92d9ab532e3ba1b038d0f
František Bráblík <frantisek.brablik@gmail.com>
2026-03-19 22:51:35 +0100

    Feat(audio): Sound groups prototype

--- engine/src/frog/core/audio/audio.cpp
+++ engine/src/frog/core/audio/audio.cpp
@@ -1,8 +1,44 @@
 #ifndef NOT_FROG_BUILD_2D
 
 #include "frog/core/audio/audio.hpp"
+#include "frog/utils/todo.hpp"
+
+#include <vector>
+#include <random>
 
 namespace frog {
+
+bool Group::add_sound(const std::string& tag, const std::string& path)
+{
+    // TODO: What if sound of tag exists but is different path?
+    // Exception?
+    sounds.push_back(tag);
+    return audio->add_sound(tag, path);
+}
+
+bool Group::remove_sound(const std::string& tag)
+{
+    std::erase(sounds, tag);
+    return audio->remove_sound(tag);
+}
+
+void Group::quick_play()
+{
+    if (sounds.empty())
+        return;
+
+    static std::random_device rd{};
+    static thread_local std::mt19937 gen{ rd() };
+
+    auto dist = std::uniform_int_distribution<size_t>(0, sounds.size() - 1);
+
+    audio->quick_play(sounds[ dist(gen) ]);
+}
+
+void Group::play(lib2d::Channel& chn)
+{
+    TODO();
+}
 
 bool Audio::add_sound(const std::string& tag, const std::string& path)
 {
--- engine/src/frog/core/audio/audio.hpp
+++ engine/src/frog/core/audio/audio.hpp
@@ -9,8 +9,30 @@
 
 #include <string>
 #include <utility>
+#include <vector>
 
 namespace frog {
+
+class Audio;
+
+class Group
+{
+    Audio* audio = nullptr;
+    std::vector<std::string> sounds;
+
+public:
+    Group(Audio& audio)
+        : audio(&audio)
+    { }
+
+    bool add_sound(const std::string& tag, const std::string& path);
+
+    bool remove_sound(const std::string& tag);
+
+    void quick_play();
+
+    void play(lib2d::Channel& chn);
+};
 
 class Audio
 {
@@ -22,15 +44,33 @@ class Audio
 
     gx::assets<lib2d::Sound> sounds;
 
+    gx::assets<ptr<Group>> groups;
+
     static constexpr int FREQUENCY = 22050;
     static constexpr int CHANNELS = 2;
     static constexpr int CHUNK_SIZE = 1024;
+
+    static constexpr int SPARE_CHANNELS = 8;
 
 public:
     Audio(std::string path_prefix)
         : path_prefix(std::move(path_prefix))
         , audio(FREQUENCY, CHANNELS, CHUNK_SIZE)
     { }
+
+    Group* group_at(const std::string& tag)
+    {
+        auto* ptr = groups.find(tag);
+        if (not ptr)
+            return nullptr;
+        return ptr->get();
+    }
+
+    Group* add_group(const std::string& tag)
+    {
+        groups.add(tag, mk_ptr<Group>(*this));
+        return groups.at(tag).get();
+    }
 
     bool add_sound(const std::string& tag, const std::string& path);
 
