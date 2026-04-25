c08786764b3a83ba408a90093f37d04448d22344
František Bráblík <frantisek.brablik@gmail.com>
2026-04-25 12:43:25 +0200

    Feat(utils): Exception stacktrace conditional ::backtrace

--- engine/src/frog/utils/exception.cpp
+++ engine/src/frog/utils/exception.cpp
@@ -1,28 +1,11 @@
 #include "exception.hpp"
 
-#include "frog/utils/assert.hpp"
-#include "frog/utils/string.hpp"
-#include "frog/utils/string_builder.hpp"
-
-#ifdef FROG_USE_STACKTRACE
-    #include <version>
-    #if __has_include(<stacktrace>)
-        #if __cpp_lib_stacktrace >= 202011L
-            #include <stacktrace>
-            #define FROG_HAS_STACKTRACE
-        #endif
-    #endif
-#endif
-
-#include <execinfo.h>   // Linux-specific, backtrace, backtrace_symbols
-
-#include <cxxabi.h>
-
-#include <array>
 #include <memory>
-#include <cstdlib>
-#include <string>
-#include <string_view>
+
+#if __has_include(<cxxabi.h>)
+    #include <cxxabi.h>     // abi::__cxa_demangle
+    #define FROG_HAS_DEMANGLE
+#endif
 
 namespace {
 
@@ -41,6 +24,9 @@ struct Free
 
 bool demangle(std::string& mangled)
 {
+#ifndef FROG_HAS_DEMANGLE
+    return true;
+#else
     int status = 0;
     auto demangled = std::unique_ptr<char[], Free>{};
     demangled.reset( abi::__cxa_demangle(mangled.c_str(), nullptr, nullptr, &status) );
@@ -53,40 +39,95 @@ bool demangle(std::string& mangled)
 
     mangled = std::string(demangled.get());
     return true;
+#endif
 }
 
 } // namespace
 
-namespace frog {
+#ifdef FROG_USE_STACKTRACE
+    #include <version>
+    #if __has_include(<stacktrace>)
+        #include <stacktrace>
+        #pragma message("ahoj")
+        #if __cpp_lib_stacktrace >= 202011L
+            #define FROG_HAS_STACKTRACE 1
+            #define FROG_STACKTRACE_STD 1
+            #pragma message("std::stacktrace")
+        #endif
+    #endif
 
-void error::init_stacktrace()
-{
-#ifdef FROG_HAS_STACKTRACE
-    stacktrace = std::to_string( std::stacktrace::current() );
+    #if !defined(FROG_STACKTRACE_STD) && __has_include(<execinfo.h>)
+        #include <execinfo.h>   // Linux-specific, backtrace, backtrace_symbols
+
+        #define FROG_HAS_STACKTRACE 1
+        #pragma message("::backtrace")
+    #endif
+
 #endif
 
-    auto trace = std::array<void*, 256>{};
-    int size = ::backtrace(trace.data(), trace.size());
+#ifndef FROG_HAS_STACKTRACE
 
-    auto lines = std::unique_ptr<char*, Free>{};
-    lines.reset( ::backtrace_symbols(trace.data(), size) );
+    namespace frog {
 
-    stacktrace.clear();
-
-    for (int i = 0; i < size; i++)
+    void error::init_stacktrace()
     {
-        auto full = std::string_view(lines.get()[i]);
-
-        auto func_name = std::string( frog::between(full, '(', '+') );
-        demangle(func_name);
-
-        auto lib = frog::basename( std::string( full.substr(0, full.find('(')) ) );
-        auto pos = std::string( frog::between(full, '+', ')') );
-        auto ptr = std::string( frog::between(full, '[', ']') );
-
-        stacktrace += frog::make_string(lib, " ( ", func_name, " +", pos, " ) [ ", ptr, " ]\n");
-        // LOG(frog::make_string(lib, " | ", func_name, " | ", pos, " | ", ptr));
+        // noop
     }
-}
 
-} // namespace frog
+    } // namespace frog
+
+#else
+
+    #if defined(FROG_STACKTRACE_STD)
+
+        namespace frog {
+
+        void error::init_stacktrace()
+        {
+            stacktrace = "std::stacktrace " + std::to_string( std::stacktrace::current() );
+        }
+
+        } // namespace frog
+
+    #else
+        #include "frog/utils/assert.hpp"
+        #include "frog/utils/string.hpp"
+        #include "frog/utils/string_builder.hpp"
+
+        #include <array>
+        #include <string_view>
+        #include <cstdlib>
+        #include <string>
+
+        namespace frog {
+
+        void error::init_stacktrace()
+        {
+            auto trace = std::array<void*, 256>{};
+            int size = ::backtrace(trace.data(), trace.size());
+
+            auto lines = std::unique_ptr<char*, Free>{};
+            lines.reset( ::backtrace_symbols(trace.data(), size) );
+
+            stacktrace.clear();
+
+            for (int i = 0; i < size; i++)
+            {
+                auto full = std::string_view(lines.get()[i]);
+
+                auto func_name = std::string( frog::between(full, '(', '+') );
+                demangle(func_name);
+
+                auto lib = frog::basename( std::string( full.substr(0, full.find('(')) ) );
+                auto pos = std::string( frog::between(full, '+', ')') );
+                auto ptr = std::string( frog::between(full, '[', ']') );
+
+                stacktrace += frog::make_string(lib, " ( ", func_name, " +", pos, " ) [ ", ptr, " ]\n");
+                // LOG(frog::make_string(lib, " | ", func_name, " | ", pos, " | ", ptr));
+            }
+        }
+
+        } // namespace frog
+
+    #endif
+#endif
