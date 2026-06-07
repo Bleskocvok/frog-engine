ddb3855b33eaf9ff1e37ca90a51d40c18b23b86e
František Bráblík <frantisek.brablik@gmail.com>
2026-06-07 14:58:33 +0200

    Fix(scripts): Profiler display transparent bg

--- engine/src/frog/scripts/profiler_display.hpp
+++ engine/src/frog/scripts/profiler_display.hpp
@@ -3,7 +3,9 @@
 #include "frog/core/script.hpp"
 #include "frog/core/engine2d.hpp"
 #include "frog/geometry/vector.hpp"
+#include "frog/graphics/color.hpp"
 #include "frog/graphics/ui_element.hpp"
+#include "frog/gx2d/sprite.hpp"
 #include "frog/scripts/profiler_script.hpp"
 #include "frog/utils/string_builder.hpp"
 
@@ -25,11 +27,6 @@ public:
         : start(start)
     { }
 
-    void init(frog::game_object2d& obj, frog::engine2d&) override
-    {
-
-    }
-
     void frame_update(frog::game_object2d& obj, frog::engine2d& e) override
     {
         for (auto* elem : elems)
@@ -42,21 +39,35 @@ public:
         if (not profiler)
             return;
 
+        {
+            auto* bg = obj.add_element(frog::mk_ptr<frog::gx::ui_element>());
+            elems.push_back(bg);
+
+            bg->sprite.image_tag = "box";
+            bg->sprite.color = { 0, 0, 0, 128 };
+
+            bg->sprite.rect.size = { 1, label_height * profiler->times().size() * 2 };
+            bg->sprite.rect.pos = start;
+            bg->sprite.rect.pos.y() += 0.5 * label_height * profiler->times().size();
+        }
+
         for (const auto& t : profiler->times())
         {
             auto* ui = obj.add_element(frog::mk_ptr<frog::gx::ui_element>());
+            elems.push_back(ui);
+
             ui->label = { "text:", 1 };
+            ui->label->align = decltype(ui->label->align)::CENTER;
             ui->pos() = pos;
             ui->size() = { 0, label_height };
             // TODO: Fuck.
             ui->sprite.layer = 100000;
-            pos.y() += 0.025;
 
             std::ostringstream o;
             profiler->out_line(o, t);
-            ui->label->str = frog::make_string("| ", std::move(o).str());
+            ui->label->str = frog::make_string("|", std::move(o).str());
 
-            elems.push_back(ui);
+            pos.y() += 0.025;
         }
     }
 };
