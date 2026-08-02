9781a868e597c9f05c5ea983ee458c07a2690fd9
František Bráblík <frantisek.brablik@gmail.com>
2026-08-02 22:25:33 +0200

    Feat(utils): ProfilerGuard parentage

--- engine/src/frog/utils/profiler_guard.hpp
+++ engine/src/frog/utils/profiler_guard.hpp
@@ -1,18 +1,55 @@
 #pragma once
 
+#include "frog/utils/exception.hpp"
 #include "frog/utils/string.hpp"
 #include "frog/os/timer.hpp"
 
 #include <cstdint>
+#include <optional>
 #include <source_location>
 #include <string>
 #include <string_view>
 #include <unordered_map>
 #include <utility>      // move
+#include <vector>
 
 #define FROG_PROFILE_FUNC() auto guard = ::frog::ProfilerGuard()
 
 namespace frog {
+
+namespace detail {
+
+class ProfilerStack
+{
+public:
+    void push(std::string id)
+    {
+        data.push_back(std::move(id));
+    }
+
+    void pop()
+    {
+        data.pop_back();
+    }
+
+    bool empty() const
+    {
+        return data.empty();
+    }
+
+    const auto& top() const
+    {
+        if (empty())
+            throw error("ProfilerStack::top(): empty");
+
+        return data.back();
+    }
+
+private:
+    std::vector<std::string> data;
+};
+
+} // namespace detail
 
 class ProfilerGuard
 {
@@ -49,13 +86,13 @@ public:
     ProfilerGuard(std::string name)
         : name(std::move(name))
     {
-        timer.reset();
+        init();
     }
 
     ProfilerGuard(const std::source_location& location = std::source_location::current())
         : name( strip( location.function_name() ) )
     {
-        timer.reset();
+        init();
     }
 
     ProfilerGuard(const ProfilerGuard&) = delete;
@@ -64,6 +101,8 @@ public:
     ~ProfilerGuard()
     {
         times_us[name].put( timer.duration_us() );
+
+        stack.pop();
     }
 
     static std::string strip(const char* str)
@@ -88,14 +127,32 @@ public:
         return times_us;
     }
 
+    std::optional<std::string> parent() const
+    {
+        return parent_;
+    }
+
 private:
     static inline std::unordered_map<std::string, Item> times_us;
+    static inline detail::ProfilerStack stack;
 
     std::string name;
 
     using Timer = frog::os::timer;
 
     Timer timer;
+
+    std::optional<std::string> parent_;
+
+    void init()
+    {
+        timer.reset();
+
+        if (not stack.empty())
+            parent_ = stack.top();
+
+        stack.push(name);
+    }
 };
 
 } // namespace frog
